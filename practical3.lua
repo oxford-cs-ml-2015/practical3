f906cfa9182cbd6212f52678a4699a1eaa8e23d0
Brendan Shillingford <brendan.shillingford@cs.ox.ac.uk>
2015-02-13 07:31:37 +0000

    clean up code, clarify things

--- practical3.lua
+++ practical3.lua
@@ -1,5 +1,8 @@
 ---------------------------------------------------------------------------------------
 -- Practical 3 - Learning to use different optimizers with logistic regression
+--
+-- to run: th -i practical3.lua
+-- or:     luajit -i practical3.lua
 ---------------------------------------------------------------------------------------
 
 require 'torch'
@@ -15,15 +18,19 @@ require 'dataset-mnist'
 
 torch.manualSeed(1)    -- fix random seed so program runs the same every time
 
+-- TODO: play with these optimizer options for the second handin item, as described in the writeup
 -- NOTE: see below for optimState, storing optimiser settings
 local opt = {}         -- these options are used throughout
 opt.optimization = 'sgd'
-opt.batch_size = 5
+opt.batch_size = 3
 opt.train_size = 8000  -- set to 0 or 60000 to use all 60000 training data
 opt.test_size = 0      -- 0 means load all data
+opt.epochs = 2         -- **approximate** number of passes through the training data (see below for the `iterations` variable, which is calculated from this)
 
-local optimState
-local optimMethod
+-- NOTE: the code below changes the optimization algorithm used, and its settings
+local optimState       -- stores a lua table with the optimization algorithm's settings, and state during iterations
+local optimMethod      -- stores a function corresponding to the optimization routine
+-- remember, the defaults below are not necessarily good
 if opt.optimization == 'lbfgs' then
   optimState = {
     learningRate = 1e-1,
@@ -66,10 +73,10 @@ local function load_dataset(train_or_test, count)
     data.data = data.data:index(1, shuffled_indices):squeeze()
     data.labels = data.labels:index(1, shuffled_indices):squeeze()
 
-    -- UNCOMMENT to display a training example
+    -- TODO: (optional) UNCOMMENT to display a training example
     -- for more, see torch gnuplot package documentation:
     -- https://github.com/torch/gnuplot#plotting-package-manual-with-gnuplot
-    --gnuplot.imagesc(data.data[20])
+    --gnuplot.imagesc(data.data[10])
 
     -- vectorize each 2D data point into 1D
     data.data = data.data:reshape(data.data:size(1), 32*32)
@@ -90,7 +97,7 @@ local test = load_dataset('test', opt.test_size)
 -- MODEL
 ------------------------------------------------------------------------------
 
-local n_data = train.data:size(1)       -- number of training data
+local n_train_data = train.data:size(1) -- number of training data
 local n_inputs = train.data:size(2)     -- number of cols = number of dims of input
 local n_outputs = train.labels:max()    -- highest label = # of classes
 
@@ -99,7 +106,7 @@ print(train.labels:min())
 
 local lin_layer = nn.Linear(n_inputs, n_outputs)
 local softmax = nn.LogSoftMax() 
-model = nn.Sequential()
+local model = nn.Sequential()
 model:add(lin_layer)
 model:add(softmax)
 
@@ -125,13 +132,23 @@ local feval = function(x)
     parameters:copy(x)
   end
 
-  -- get start/end indices for our minibatch (here called "batch")
+  -- get start/end indices for our minibatch (in this code we'll call a minibatch a "batch")
+  --           ------- 
+  --          |  ...  |
+  --        ^ ---------<- start index = i * batchsize + 1
+  --  batch | |       |
+  --   size | | batch |       
+  --        v |   i   |<- end index (inclusive) = start index + batchsize
+  --          ---------                         = (i + 1) * batchsize + 1
+  --          |  ...  |                 (except possibly for the last minibatch, we can't 
+  --          --------                   let that one go past the end of the data, so we take a min())
   local start_index = counter * opt.batch_size + 1
-  local end_index = math.min(n_data, (counter + 1) * opt.batch_size)
-  if end_index == n_data then
+  local end_index = math.min(n_train_data, (counter + 1) * opt.batch_size + 1)
+  if end_index == n_train_data then
     counter = 0
+  else
+    counter = counter + 1
   end
-  counter = counter + 1
 
   local batch_inputs = train.data[{{start_index, end_index}, {}}]
   local batch_targets = train.labels[{{start_index, end_index}}]
@@ -153,39 +170,58 @@ local feval = function(x)
 end
   
 ------------------------------------------------------------------------
--- Optimize 
+-- OPTIMIZE: FIRST HANDIN ITEM
 ------------------------------------------------------------------------
-local epochs = 10  -- number of full passes over all the training data
-local losses = {}
-local epoch_loss = 0  
-for epoch = 1, epochs do
-  epoch_loss = 0
+local losses = {}          -- training losses for each iteration/minibatch
+local epochs = opt.epochs  -- number of full passes over all the training data
+local iterations = epochs * math.ceil(n_train_data / opt.batch_size) -- integer number of minibatches to process
+-- (note: number of training data might not be divisible by the batch size, so we round up)
 
-  -- loop over however many minibatches there are (by convention _ means a variable we ignore)
-  for _ = 1, train.data:size(1), opt.batch_size do
-    -- remember optimMethod is something like optim.sgd/optim.adagrad, see top of this file
-    local __, minibatch_loss = optimMethod(feval, parameters, optimState)
-    epoch_loss = epoch_loss + minibatch_loss[1]
+-- In each iteration, we:
+--    1. call the optimization routine, which
+--      a. calls feval(parameters), which
+--          i. grabs the next minibatch
+--         ii. returns the loss value and the gradient of the loss wrt the parameters, evaluated on the minibatch
+--      b. the optimization routine uses this gradient to adjust the parameters so as to reduce the loss.
+--    3. then we append the loss to a table (list) and print it
+for i = 1, iterations do
+  -- optimMethod is a variable storing a function, either optim.sgd or optim.adagrad or ...
+  -- see documentation for more information on what these functions do and return:
+  --   https://github.com/torch/optim
+  -- it returns (new_parameters, table), where table[0] is the value of the function being optimized
+  -- and we can ignore new_parameters because `parameters` is updated in-place every time we call 
+  -- the optim module's function. It uses optimState to hide away its bookkeeping that it needs to do
+  -- between iterations.
+  local _, minibatch_loss = optimMethod(feval, parameters, optimState)
+
+  -- Our loss function is cross-entropy, divided by the number of data points,
+  -- therefore the units (units in the physics sense) of the loss is "loss per data sample".
+  -- Since we evaluate the loss on a different minibatch each time, the loss will sometimes 
+  -- fluctuate upwards slightly (i.e. the loss estimate is noisy).
+  if i % 10 == 0 then -- don't print *every* iteration, this is enough to get the gist
+      print(string.format("minibatches processed: %6s, loss = %6.6f", i, minibatch_loss[1]))
   end
 
-  losses[#losses+1] = epoch_loss
-  print("epoch = " .. epoch .. ', full epoch loss = ' .. epoch_loss)
+  losses[#losses + 1] = minibatch_loss[1] -- append the new loss
 end
 
--- turn list of losses into a Tensor, and plot it
+-- Turn table of losses into a torch Tensor, and plot it
+-- TODO: for the first handin item, evaluate test loss above, and add to the plot below
 gnuplot.plot({
-  torch.range(1, #losses),        -- x-coordinates for data to plot
-  torch.Tensor(losses),           -- y-coordinates
+  torch.range(1, #losses),        -- x-coordinates for data to plot, creates a tensor holding {1,2,3,...,#losses}
+  torch.Tensor(losses),           -- y-coordinates (the training losses)
   '-'})
 
+print(string.format('%e',torch.min(torch.Tensor(losses), 1)))
+
 ------------------------------------------------------------------------------
--- TESTING THE LEARNED MODEL
+-- TESTING THE LEARNED MODEL: 2ND HANDIN ITEM
 ------------------------------------------------------------------------------
 
 local logProbs = model:forward(test.data)
 local classProbabilities = torch.exp(logProbs)
-local _, classPredictions = torch.max(classProbabilities,2)
+local _, classPredictions = torch.max(classProbabilities, 2)
+-- classPredictions holds predicted classes from 1-10
 
--- TODO: compute test error here
-
+-- TODO: compute test classification error here for the second handin item
 
